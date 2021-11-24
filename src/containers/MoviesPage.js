9ab29b7ff6861192a56592aab527f913167c9c59
bcharlotin1 <biancacharlotin1@gmail.com>
2021-11-24 16:42:28 -0500

    Done.

--- src/containers/MoviesPage.js
+++ src/containers/MoviesPage.js
@@ -1,12 +1,26 @@
 // .src/containers/MoviesPage.js
-import React from 'react';
-import { Route } from 'react-router-dom';
-import MoviesList from '../components/MoviesList';
+import React from "react";
+import { Route } from "react-router-dom";
+import MoviesList from "../components/MoviesList";
+import MovieShow from "../components/MovieShow";
 
 const MoviesPage = ({ match, movies }) => (
   <div>
     <MoviesList movies={movies} />
+  {/* // We also add a `Route` component that will render `MovieShow` // when a movie
+  is selected 
+  
+  comes from the routerProps passed in from App. This is a POJO (plain old Javascript object) that contains the current URL. Using match, we can show stuff depending on what the match.url returns
+  
+  */}
+   <Route
+      exact
+      path={match.url}
+      render={() => <h3>Choose a movie from the list above</h3>}
+    />
+    <Route path={`${match.url}/:movieId`} render={(routerProps) => <MovieShow {...routerProps} movies={movies} />} />
   </div>
+
 )
 
 export default MoviesPage
